6d282aaf8ed50ee98af313c582cd617f71302ee9
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-07 12:02:52 +0300

    organizing project arichitecture

--- client/src/services/gameServices.js
+++ client/src/services/gameServices.js
@@ -1,4 +1,4 @@
-// function for game management
+// Functions for game management
 
 export function chooseLevel(){
     return question("Choose game level (all/ multi-choices/ easy/ medium/ hard): ")
