ddac0ffd144f4ac5b95831a45d3b2fa58e413c67
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-07 15:44:44 +0300

    adding readRiddles function in riddleDAL.js, response with readRiddles if request with '/riddles'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 // Riddle Game Server
 
 import { createServer } from "http";
+import { readRiddels } from "./src/dal/riddleDAL.js";
 
 const PORT = 3000;
 
 const server = createServer();
 
+// call listen method to run server on localhost
 server.listen(PORT, () => {
     console.log(`Server is running on 'http://localhost:${PORT}'...`);
+});
+
+server.on("request", async (req, res) => {
+    switch (req.url){
+        case "/":
+            // if the request'url is the default path, response with message that the server is running
+            res.end("Server is running...");
+        case "/riddles":
+            // if the request'url is whith '/riddles'
+            res.end(await readRiddels());
+    }
 })
--- server/src/dal/riddleDAL.js
+++ server/src/dal/riddleDAL.js
@@ -2,8 +2,16 @@
 
 import fs from "fs/promises";
 
-const PATH = "../data/riddles.txt";
+const PATH = "../../data/riddles.txt";
 
 export async function readRiddels(){
-    return fs.readFile(PATH, 'utf-8');
+    try{
+        // read all riddles from riddles.txt
+        return await fs.readFile(PATH, 'utf-8');
+    }
+    catch(err){
+        // if there is an error, log to the console
+        console.error(`Error reading riddles: ${err}`);
+        return;
+    }
 }
