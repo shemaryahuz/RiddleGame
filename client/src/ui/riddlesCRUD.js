f049a19e9a3bbf64e36f9abca0b5d8eb77925c83
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-09 18:00:42 +0300

    adding basic crud on client side

--- client/src/ui/riddlesCRUD.js
+++ client/src/ui/riddlesCRUD.js
@@ -1,8 +1,9 @@
 // CRUD methods for riddles managment
 
+import Id from "short-unique-id"
 import { question } from "readline-sync";
 import Riddle from "../models/Riddle.js";
-import { getRiddles, sendRiddle } from "../services/riddleService.js";
+import { addRiddle, allRiddles, deleteRiddle, getRiddles, sendRiddle } from "../services/riddleService.js";
 
 function showRiddles(riddles){
     // function that gets an array of riddles and log the details to the console
@@ -20,17 +21,19 @@ function showRiddles(riddles){
 
 export async function showAllRiddles(){
     // function to get and show all the riddles
-    const riddles = await getRiddles();
+    // const riddles = await getRiddles();
+    const riddles = allRiddles;
     showRiddles(riddles);
 }
 
 export async function createRiddle() {
     // function to create a new riddle and store in the database
-    const riddles = await getRiddles();
+    // const riddles = await getRiddles();
+    const riddles = allRiddles;
     // get riddles from the server to create new id
     console.log("Creating a new Riddle:");
     // create Riddle properties
-    const rId = riddles.length + 1;
+    const rId = new Id().rnd(3);
     // get user inputs
     const rName = question("Enter Riddle name: ");
     const rQuestion = question("Enter Riddle question: ");
@@ -38,9 +41,16 @@ export async function createRiddle() {
     // create riddle entity
     const riddle = new Riddle({
         id: rId,
+        level: "extra",
         name: rName,
         question: rQuestion,
         answer: rAnswer
     });
-    await sendRiddle(riddle);
+    // await sendRiddle(riddle);
+    addRiddle(riddle);
+}
+
+export function deleteRiddleById(){
+    const id = question("Enter id for deleting: ");
+    deleteRiddle(id);
 }
