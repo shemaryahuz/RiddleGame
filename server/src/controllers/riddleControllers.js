89690a37ff17322f048af09b45a6ddda0e6bbe87
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-10 16:50:24 +0300

    adding controler and route for addRiddle

--- server/src/controllers/riddleControllers.js
+++ server/src/controllers/riddleControllers.js
@@ -1,6 +1,6 @@
 // handlers for riddles route
 
-import { getAllRiddles, getRiddle } from "../services/riddleService.js";
+import { addRiddle, getAllRiddles, getRiddle } from "../services/riddleService.js";
 
 export async function sendAllRiddles(req, res) {
     // get all riddles array
@@ -18,9 +18,30 @@ export async function sendRiddle(req, res) {
     const riddle = await getRiddle(riddleId);
     // if riddle is undefined, return message and status code of 'not found'
     if (!riddle){
-        res.status(404).send({ msg: "riddle id not found"});
+        res.status(404).send({ error: "riddle id not found" });
     }
     // if riddle found, convert to json string and send it
     const riddleStr = JSON.stringify(riddle, null, 2);
     res.send(riddleStr);
+}
+
+export async function addNewRiddle(req, res) {
+    // destruct the riddle from request body
+    const riddle = req.body;
+    // if body is not a json, return status code of bad request with error message
+    if (!riddle){
+        res.status(404).send({ error: "body is malformed" })
+    }
+    // if body is not a valid format of riddle, return status code of bad request with error message
+    if (!riddle.name || !riddle.question || !riddle.answer){
+        res.status(400).send({ error: "riddle format is invalid" })
+    }
+    // add the new riddle
+    const added = await addRiddle(riddle);
+    // if not added, response with error message and status code of error
+    if (!added){
+        res.status(500).json({ error: "error adding riddle to the database"})
+    }
+    // if added response with succeded message
+    res.json({ message: "riddle added successfuly"});
 }
--- server/src/routes/riddleRouter.js
+++ server/src/routes/riddleRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendAllRiddles, sendRiddle } from "../controllers/riddleControllers.js";
+import { addNewRiddle, sendAllRiddles, sendRiddle } from "../controllers/riddleControllers.js";
 // router for handling with riddles
 
 // initialize router
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get("/", sendAllRiddles);
 // if route is '/riddles/:riddleId', send the riddle by id
 router.get("/:riddleId", sendRiddle);
+// if route is '/riddles/addRiddle', create new riddle
+router.post("/addRiddle", addNewRiddle);
 
 export default router;
