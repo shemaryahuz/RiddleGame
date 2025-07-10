1e3f90e948fe25c6c6fd684f15025d326befc423
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-10 15:31:26 +0300

    fixing update and delete methods

--- server/src/services/riddleService.js
+++ server/src/services/riddleService.js
@@ -15,6 +15,25 @@ export async function getAllRiddles(){
     }
 }
 
+export async function getRiddle(riddleId) {
+    try {
+        // get all riddles
+        const riddles = await getAllRiddles();
+        // loop over the riddles
+        for (let riddle of riddles){
+            // if riddle found, return it
+            if (riddle.id === riddleId){
+                return riddle;
+            }
+        }
+        // if not found, return undefined
+        return;
+    } catch (error) {
+        // if there is an error, log to the console
+        console.error(`Error getting riddle: ${error}`);
+    }
+}
+
 export async function addRiddle(riddle){
     try{
         // add unique id to the riddle
@@ -24,32 +43,64 @@ export async function addRiddle(riddle){
         riddles.push(riddle);
         // convert ridles to a json string for writing to a file
         const riddlesStr = JSON.stringify(riddles, null, 2);
-        return await writeRiddles(riddlesStr);
+        const added = await writeRiddles(riddlesStr);
+        return added;
     }
     catch(err){
         // if there is an error, log to the console
-        console.error(`Error writing riddles to the data: ${err}`);
+        console.error(`Error adding riddle: ${err}`);
+        return false;
+    }
+}
+
+export async function updateRiddle(riddleId, newRiddle) {
+    try {
+        // add unique id to the new riddle
+        newRiddle.id = await generateUniqueId();
+        // get all riddles
+        const riddles = await getAllRiddles();
+        // loop over the riddles
+        for (let i = 0; i < riddles.length; i++){
+            // if riddle id found, update with the new riddle and return true if updated
+            if (riddles[i].id === riddleId){
+                riddles[i] = newRiddle;
+                // convert ridles to a json string for writing to a file
+                const riddlesStr = JSON.stringify(riddles, null, 2);
+                const updated = await writeRiddles(riddlesStr);
+                return updated;
+            }
+        }
+        // if not found, return false
+        return false;
+        
+    } catch (error) {
+        // if there is an error, log to the console
+        console.error(`Error updating riddle: ${error}`);
         return false;
     }
 }
 
 export async function deleteRiddle(riddleId) {
     try {
-            // get all riddles
-            const riddles = await getAllRiddles();
-            // loop over the riddles
-            for (let i = 0; i< riddles.length; i++){
-                // if riddle found, delete it and update in data
-                if (riddles[i].id === riddleId){
-                    riddles.splice(i, 1);
-                    // convert ridles to a json string for writing to a file
-                    const riddlesStr = JSON.stringify(riddles, null, 2);
-                    return await writeRiddles(riddlesStr);
-                }
+        // get all riddles
+        const riddles = await getAllRiddles();
+        // loop over the riddles
+        for (let i = 0; i < riddles.length; i++){
+            // if riddle id found, delete and return true if deleted
+            if (riddles[i].id === riddleId){
+                riddles.splice(i, 1);
+                // convert ridles to a json string for writing to a file
+                const riddlesStr = JSON.stringify(riddles, null, 2); 
+                const deleted = await writeRiddles(riddlesStr);
+                return deleted;
             }
-            // if not found, return false
-            return false;
-    } catch (error) {
+        }
+        // if not found, return false
+        return false;
         
+    } catch (error) {
+        // if there is an error, log to the console
+        console.error(`Error deleting riddle: ${error}`);
+        return false;
     }
 }
