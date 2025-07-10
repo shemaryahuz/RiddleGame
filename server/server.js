4c9c83de09debd2fc4e07f00a870ab5e5c203781
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-10 11:26:00 +0300

    adding basic server with folders structure

--- server/server.js
+++ server/server.js
@@ -1,42 +1,21 @@
 // Riddle Game Server
 
-import { createServer } from "http";
-import { getRiddles } from "./src/services/riddleService.js";
+import express from "express";
 
-const PORT = 3000;
+// initialize express application
+const app = express();
 
-const server = createServer();
 
-// call listen method to run server on localhost
-server.listen(PORT, () => {
-    console.log(`Server is running on 'http://localhost:${PORT}'...`);
-});
 
-server.on("request", async (req, res) => {
-    try{
-        switch(req.url){
-            // handle response according to the request's url path
 
-            case "/riddles":
-                // if the request's url is with '/riddles', send all riddles
-                const riddlesArr = await getRiddles();
-                const riddlesStr = JSON.stringify(riddlesArr);
-                res.end(riddlesStr);
 
-            case "/addRiddle":
-                // if the request's url is with '/addRiddle', add riddle
-                if (req.method.toUpperCase() === "POST"){
-                    
-                }
-            
-            default:
-                // if the request's url is the default path, response with message that the server is running
-                res.end("Server is running...");
-        }
-    }
-    catch(err){
-        // if there is an error, log to the console and response with the error;
-        console.error(err);
-        res.end(err);
-    }
+// get the port for running
+const PORT = process.env.PORT;
+
+// run the server
+app.listen(PORT, (error) => {
+    // if there is an error, log to console
+    if (error) console.error(`Error running the server: ${error}`);
+    // log message that server is running
+    console.log(`Server is running at: http://localhost:${PORT}`);
 })
