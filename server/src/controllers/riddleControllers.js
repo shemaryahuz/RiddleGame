fd94ef02f6c5e44233e4ab58cb59af238b7d34ae
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-10 15:57:56 +0300

    adding controlers and routes for all riddles and riddle by id

--- server/src/controllers/riddleControllers.js
+++ server/src/controllers/riddleControllers.js
@@ -1,9 +1,26 @@
 // handlers for riddles route
 
-import { getAllRiddles } from "../services/riddleService.js";
+import { getAllRiddles, getRiddle } from "../services/riddleService.js";
 
 export async function sendAllRiddles(req, res) {
-    // get and send a response with all riddles array
+    // get all riddles array
     const riddles = await getAllRiddles();
-    res.send(riddles);
+    // convert to organized json string
+    const riddleStr = JSON.stringify(riddles, null, 2); 
+    // send the json string
+    res.send(riddleStr);
+}
+
+export async function sendRiddle(req, res) {
+    // destruct id from request params
+    const riddleId = req.params?.riddleId;
+    // get the riddle by id
+    const riddle = await getRiddle(riddleId);
+    // if riddle is undefined, return message and status code of 'not found'
+    if (!riddle){
+        res.status(404).send({ msg: "riddle id not found"});
+    }
+    // if riddle found, convert to json string and send it
+    const riddleStr = JSON.stringify(riddle, null, 2);
+    res.send(riddleStr);
 }
--- server/src/routes/riddleRouter.js
+++ server/src/routes/riddleRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { sendAllRiddles } from "../controllers/riddleControllers.js";
+import { sendAllRiddles, sendRiddle } from "../controllers/riddleControllers.js";
 // router for handling with riddles
 
 // initialize router
 const router = express.Router();
 
+// if route is '/riddles' send all riddles
 router.get("/", sendAllRiddles);
+// if route is '/riddles/:riddleId', send the riddle by id
+router.get("/:riddleId", sendRiddle);
 
 export default router;
