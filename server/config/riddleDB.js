9f6f39bb9cbc40fa49734b8508fbe241949e9254
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-17 13:12:48 +0300

    added mongodb client for commecting riddles db

---
+++ server/config/riddleDB.js
@@ -0,0 +1,26 @@
+// mongoDB connection for riddles database
+import { MongoClient } from "mongodb";
+
+// get the uri string from .env
+const uri = process.env.MONGODB_URI
+
+// create mongodb MongoClient
+const client = new MongoClient(uri);
+
+const dbName = "riddles_db";
+
+let db;
+
+async function connectDB() {
+    // connect to the riddles database if not connected
+    if (!db){
+        await client.connect();
+        db = client.db(dbName);
+        console.log(`Connected to MongoDB. Database name: ${dbName}`);
+    }
+    return db;
+}
+
+connectDB();
+
+export default db;
