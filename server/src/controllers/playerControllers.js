c186cd5ba6a551288b83f7a6ec1def5afb0d3565
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-20 20:30:29 +0300

    added methods for updating player scores

--- server/src/controllers/playerControllers.js
+++ server/src/controllers/playerControllers.js
@@ -1,5 +1,5 @@
 // handlers for player route
-import { fetchAllPlayers, fetchPlayer, insertPlayer } from "../dal/playerDAL.js";
+import { fetchAllPlayers, fetchPlayer, fetchScores, insertPlayer, updateScores } from "../dal/playerDAL.js";
 
 export async function sendAllPlayers(req, res) {
     // get the players from the database
@@ -22,13 +22,13 @@ export async function sendPlayer(req, res) {
     // get the player by username
     try {
         const player = await fetchPlayer(username);
-        // if player array is empty, response message and status code of 'not found'
-        if (!player.length){
+        // if player is null, response message and status code of 'not found'
+        if (!player){
             res.status(404).send({ error: "player username not found" });
             return;
         }
         // if found, response with the player
-        res.send(player[0]);
+        res.send(player);
 
     } catch (error) {
         res.status(404).send({ error: `Error fetcing player: ${error.message}` });
@@ -37,18 +37,41 @@ export async function sendPlayer(req, res) {
 
 export async function addNewPlayer(req, res) {
     try {
-        // destruct the player from request body
+        // destruct the username from request body
         const { username } = req.body;
-        // if body format is invalid, response with error
-        if (!username){
-            res.status(404).send({ error: "body is malformed" });
-            return;
-        }
         // add the new player and get the created player with its id
         const createdPlayer = await insertPlayer(username);
         // if added, response with succeded message and the created player
         res.send({ message: "player added successfuly", player: createdPlayer });
     } catch (error) {
-        res.status(404).json({ error: `Error adding player: ${error.message}` });
+        res.status(404).send({ error: `Error adding player: ${error.message}` });
+    }
+}
+
+export async function updatePlayerScores(req, res) {
+    try {
+            // destruct username from requests body
+            const { username } = req.body;
+            // fetch current score and current best time
+            const currentScores = await fetchScores(username);
+            // if currentScores is null, response with 'not found'
+            if (!currentScores){
+                res.status(404).send({ error: "username not found" });
+                return;
+            }
+            const curScore = currentScores.score;
+            const curBestTime = currentScores.best_time;
+            // destruct score and time from request body
+            let { score, bestTime } = req.body;
+            score += curScore;
+            // if current best time is less than the new beast time, keep  best time with the same value
+            if (curBestTime !== 0 && curBestTime < bestTime){
+                bestTime = curBestTime;
+            }
+            // update player scores
+            const updatedPalyer = await updateScores(username, score, bestTime);
+            res.send({ message: "player updated successfully", player: updatedPalyer });
+    } catch (error) {
+        res.status(404).send({ error: `Error updating scores: ${error.message}` });
     }
 }
--- server/src/routes/playerRouter.js
+++ server/src/routes/playerRouter.js
@@ -1,6 +1,8 @@
+// router for endpoints of playeres route
 import express from "express";
-import { addNewPlayer, sendAllPlayers, sendPlayer } from "../controllers/playerControllers.js";
-// router for handling with playeres
+import { addNewPlayer, sendAllPlayers, sendPlayer, updatePlayerScores } from "../controllers/playerControllers.js";
+import { validateScores, validateUsername } from "../middlewares/playerMiddlewares.js";
+
 
 // initialize router
 const router = express.Router();
@@ -12,6 +14,9 @@ router.get("/", sendAllPlayers);
 router.get("/:username", sendPlayer);
 
 // if route is '/players/addPlayer' with POST method, create player
-router.post("/addPlayer", addNewPlayer);
+router.post("/addPlayer", validateUsername, addNewPlayer);
+
+// if route is '/players/updateScore/:username' with PUT method, update player's best time and score
+router.put("/updateScores/", validateUsername, validateScores, updatePlayerScores);
 
 export default router;
