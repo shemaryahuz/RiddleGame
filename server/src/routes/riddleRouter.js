27b7854fc7b4fd6ae16d6fd9ba4a08fc1cdf15d6
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-27 22:55:37 +0300

    added authentication and authorization middlewares to riddle router

--- server/src/routes/riddleRouter.js
+++ server/src/routes/riddleRouter.js
@@ -1,26 +1,67 @@
 // router for endpoints of riddles route
 
 import express from "express";
-import { addNewRiddle, deleteRiddleById, sendAllRiddles, sendRiddle, updateRiddleById } from "../controllers/riddleController.js";
-import { validateRiddle, validateRiddleId } from "../middlewares/riddleMiddleware.js";
+
+import {
+    authenticateToken,
+    authorizeRole
+} from "../middlewares/authMiddleware.js";
+
+import {
+    addNewRiddle,
+    deleteRiddleById,
+    sendAllRiddles,
+    sendRiddle,
+    updateRiddleById
+} from "../controllers/riddleController.js";
+
+import {
+    validateRiddle,
+    validateRiddleId
+} from "../middlewares/riddleMiddleware.js";
 
 
 // initialize router
 const router = express.Router();
 
 // if route is '/riddles' validate and send all riddles
-router.get("/", sendAllRiddles);
+router.get("/",
+    authenticateToken,
+    authorizeRole("user", "admin"),
+    sendAllRiddles
+);
 
 // if route is '/riddles/:riddleId', send the riddle by id
-router.get("/:riddleId", validateRiddleId , sendRiddle);
+router.get("/:riddleId",
+    authenticateToken,
+    authorizeRole("user", "admin"),
+    validateRiddleId, 
+    sendRiddle
+);
 
 // if route is '/riddles/addRiddle', create new riddle
-router.post("/addRiddle", validateRiddle, addNewRiddle);
+router.post("/addRiddle",
+    authenticateToken,
+    authorizeRole("user", "admin"),
+    validateRiddle,
+    addNewRiddle
+);
 
 // if route is '/riddles/:riddleId' with PUT method, update the riddle by id
-router.put("/:riddleId", validateRiddleId, validateRiddle, updateRiddleById);
+router.put("/:riddleId",
+    authenticateToken,
+    authorizeRole("admin"),
+    validateRiddleId,
+    validateRiddle,
+    updateRiddleById
+);
 
 // if route is '/riddles/:riddleId' with DELETE method, delete the riddle by id
-router.delete("/:riddleId", validateRiddleId, deleteRiddleById);
+router.delete("/:riddleId",
+    authenticateToken,
+    authorizeRole("user", "admin"),
+    validateRiddleId,
+    deleteRiddleById
+);
 
 export default router;
